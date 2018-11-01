7c42dbbc0b383bd2f05bdfcd2987cdae500859c8
smallstrong0 <393019766@qq.com>
2018-11-01 10:03:41 +0800

    【v0.0.1】list  item

--- src/pages/spider/spider.vue
+++ src/pages/spider/spider.vue
@@ -2,20 +2,18 @@
   <div class="spider">
     <ul id="example-1">
       <li v-for="item in items" :key="item.title">
-        {{ item.title }}
+        <a :href="item.link">{{ item.title }}</a>
+        <p>{{ item.desc }}</p>
+        <div>
+          <p> {{ item.niceDate }} </p>
+          <p> {{ item.author }} </p>
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-// export default {
-//   data() {
-//     return {
-//       items: [{ message: 'Foo' }, { message: 'Bar' }]
-//     }
-//   }
-// }
 
 import listProject from '../../api/blog'
 export default {
